298e010645de97ef290d5587c2e21ae8fd1081c8
Aleksandra1001 <119792463+Aleksandra1001@users.noreply.github.com>
2023-07-13 20:38:47 +0200

    Revert "Media"

--- src/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $("a[href]").on("click", function (event) {
-    event.preventDefault();
-    var id = $(this).attr("href"),
-    top = $(id).offset().top;
-    $("body,html").animate({ scrollTop: top }, 1500);
-    });
-}); 
