501dbbbb7ae0985231a24af26092ad71224bd652
Aleksandra1001 <allandra1029384756@gmail.com>
2023-07-17 16:25:19 +0200

    add media

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,22 @@ $(document).ready(function () {
     $("body,html").animate({ scrollTop: top }, 1500);
     });
 }); 
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.header__menu'),
+    menuItem = document.querySelectorAll('.header__item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('header__menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('header__menu_active');
+        })
+    })
+})
+
